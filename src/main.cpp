ccc364b779154e2caa5ab706159252520a25ae15
JoshCrouch <65277445+JoshCrouch@users.noreply.github.com>
2024-05-08 17:04:59 +1000

    Give up

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(1, OUTPUT); //4.1
+  pinMode(43, OUTPUT); //4.2
+  pinMode(4, OUTPUT); //4.3
+  pinMode(6, OUTPUT); //4.4
 }
 
+int pins[] = {1,43,4,6};
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 4; i++){
+    digitalWrite(pins[i], HIGH);
+    delay(100);
+    digitalWrite(pins[i], LOW);
+    delay(200);
+  }
 }
