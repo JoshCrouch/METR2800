c85e9ee32c0fc0aa869c201df627438064b85494
lachlamp <lachlan.potter@uqconnect.edu.au>
2024-05-17 11:28:07 +1000

    back to last night fuck

--- src/main.cpp
+++ src/main.cpp
@@ -3,110 +3,86 @@
 #include <Arm.h>
 #include <ServoDriver.h>
 
-// Define an array of Arm objects
-Arm arms[] = {
-    Arm(1, 0, 2),
-    /*
-    Arm(2, 0, 3),
-    Arm(3, 0, 4),
-    Arm(4, 0, 5),
-    Arm(5, 0, 6),
-    Arm(6, 0, 7)
-    */
-};
-
-// Calculate the number of arms
-const int numArms = sizeof(arms) / sizeof(arms[0]);
+Arm arm1(1, 0, 2); // ROT 0, EXT, 1
 
 int currentDirectionRotation = 1;
 int currentDirectionExtension = 1;
 
-const int ext = 2; // Extension
-const int ret = 0; // Rotation
-const int clo = 4; // Clockwise
-const int ant = 5; // Anti-Clockwise
+// const int ext = 2; // Extension
+// const int ret = 0; // Rotation
+// const int clo = 4; // Clockwise
+// const int ant = 5; // Anti-Clockwise
 
 // Flags for ISR-triggered stops
-volatile bool stopExtension = false;
-volatile bool stopRotation = false;
+// volatile bool stopExtension = false;
+// volatile bool stopRotation = false;
 
 // Forward declarations for the ISR functions
-void stopExt();
-void stopRet();
-void stopClo();
-void stopAnt();
+// void stopExt();
+// void stopRet();
+// void stopClo();
+// void stopAnt();
 
 void setup() {
   // Initialise the PWMServoDriver
-  Serial.begin(115200);
-  Serial.print("begin");
   startServoDriver();
-
-  
+  Serial.begin(115200);
   pinMode(LS41, INPUT);
   pinMode(LS42, INPUT);
 
-  // Set up limit switch pins as input with internal pull-up
-  pinMode(ext, INPUT_PULLUP); 
-  pinMode(ret, INPUT_PULLUP); 
-  pinMode(clo, INPUT_PULLUP); 
-  pinMode(ant, INPUT_PULLUP);
 
-  // Initialise limit switches
-  attachInterrupt(digitalPinToInterrupt(ext), stopExt, FALLING);
-  attachInterrupt(digitalPinToInterrupt(ret), stopRet, FALLING);
-  attachInterrupt(digitalPinToInterrupt(clo), stopClo, FALLING);
-  attachInterrupt(digitalPinToInterrupt(ant), stopAnt, FALLING);
+  // Set up limit switch pins as input with internal pull-up
+  // pinMode(ext, INPUT_PULLUP); 
+  // pinMode(ret, INPUT_PULLUP); 
+  // pinMode(clo, INPUT_PULLUP); 
+  // pinMode(ant, INPUT_PULLUP);
+
+  // // Initialise limit switches
+  // attachInterrupt(digitalPinToInterrupt(ext), stopExt, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(ret), stopRet, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(clo), stopClo, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(ant), stopAnt, FALLING);
+
+
 
   delay(10);
 }
 
 void manageServoDirection() {
-  // Read the state of the limit switches
+  // Read the state of the limit switch
   int limitSwitchStaterotation = digitalRead(LS41);
   int limitSwitchStateExtension = digitalRead(LS42);
 
-  // Debug output for limit switch states
-  Serial.print("LS41: ");
-  Serial.println(limitSwitchStaterotation);
-  Serial.print("LS42: ");
-  Serial.println(limitSwitchStateExtension);
 
-  // Manage rotation based on the limit switch state
+  // Check if the limit switch is triggered (HIGH)
   if (limitSwitchStaterotation == HIGH) {
+    // If currently moving clockwise, change to anticlockwise
     if (currentDirectionRotation == 1) {
       currentDirectionRotation = -1;
-      for (int i = 0; i < numArms; i++) {
-        arms[i].setRotation(currentDirectionRotation, 100);
-      }
+      arm1.setRotation(currentDirectionRotation, 100);
       Serial.println("Changing to anticlockwise rotation");
     }
   } else {
+    // If currently moving anticlockwise, change to clockwise
     if (currentDirectionRotation == -1) {
       currentDirectionRotation = 1;
-      for (int i = 0; i < numArms; i++) {
-        arms[i].setRotation(currentDirectionRotation, 100);
-      }
+      arm1.setRotation(currentDirectionRotation, 100);
       Serial.println("Changing to clockwise rotation");
     }
   }
-
-  // Manage extension based on the limit switch state
   if (limitSwitchStateExtension == HIGH) {
+    // If currently moving clockwise, change to anticlockwise
     if (currentDirectionExtension == 1) {
       currentDirectionExtension = -1;
-      for (int i = 0; i < numArms; i++) {
-        arms[i].setExtension(currentDirectionExtension, 100);
-      }
-      Serial.println("Changing to retraction");
+      arm1.setExtension(currentDirectionExtension, 100);
+      Serial.println("Changing to anticlockwise rotation");
     }
   } else {
+    // If currently moving anticlockwise, change to clockwise
     if (currentDirectionExtension == -1) {
       currentDirectionExtension = 1;
-      for (int i = 0; i < numArms; i++) {
-        arms[i].setExtension(currentDirectionExtension, 100);
-      }
-      Serial.println("Changing to extension");
+      arm1.setExtension(currentDirectionExtension, 100);
+      Serial.println("Changing to clockwise rotation");
     }
   }
 }
@@ -115,75 +91,47 @@ void loop() {
   // Manage the servo direction based on the limit switch state
   manageServoDirection();
   delay(50); // Small delay to prevent too rapid switching
+}
 
-  // Uncommented original loop logic adapted for array of arms
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setRotation(0, 0);
-    arms[i].setExtension(1, 100);
-  }
+// void loop() {
+//   arm1.setRotation(0, 0);
+//   //arm1.setExtension(1, 100);
+//    if (digitalRead(LS41) == HIGH) {
+//     arm1.setRotation(-1, 100);
+//   //   arm1.setExtension(0, 0);
+//   }
+//    if (digitalRead(LS42) == HIGH) {
+//      arm1.setRotation(1, 100);
+//   }
+  // delay (500);
+  //   Serial.println("Speed: 0");
+  //   arm1.setRotation(0, 0);
+  //  // arm1.setExtension(0, 0);
+  //   delay(1000);
 
+  //   Serial.println("Speed: 100");
+  //   arm1.setRotation(1, 100);
+  //   //arm1.setExtension(-1, 100);
+  //   delay(500);
 
-  if (digitalRead(LS41) == HIGH) {
-    for (int i = 0; i < numArms; i++) {
-      arms[i].setRotation(-1, 100);
-      arms[i].setExtension(0, 0);
-    }
+  //   Serial.println("Speed: 0");
+  //   arm1.setRotation(0, 0);
+  //   //arm1.setExtension(0, 0);
+  //   delay(1000);
   
-  }
-  if (digitalRead(LS42) == HIGH) {
-    for (int i = 0; i < numArms; i++) {
-      arms[i].setRotation(1, 100);
-      arms[i].setExtension(0, 0);
-    }
-  }
-
-  delay(500);
-  Serial.println("Speed: 0");
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setRotation(0, 0);
-    arms[i].setExtension(0, 0);
-  }
-  delay(1000);
-
-  Serial.println("Speed: 100");
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setRotation(1, 100);
-    arms[i].setExtension(-1, 100);
-  }
-  delay(500);
-
-  Serial.println("Speed: 0");
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setRotation(0, 0);
-    arms[i].setExtension(0, 0);
-  }
-  delay(1000);
-}
-
-void stopExt() {
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setExtension(0, 0);
-  }
-  stopExtension = true;
-}
-
-void stopRet() {
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setExtension(0, 0);
-  }
-  stopExtension = true;
-}
-
-void stopClo() {
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setRotation(0, 0);
-  }
-  stopRotation = true;
-}
-
-void stopAnt() {
-  for (int i = 0; i < numArms; i++) {
-    arms[i].setRotation(0, 0);
-  }
-  stopRotation = true;
-}
+// void stopExt(){
+//   arm1.setExtension(0,0);
+//   stopRotation = true;
+// }
+// void stopRet(){
+//   arm1.setExtension(0,0);
+//   stopRotation = true;
+// }
+// void stopClo(){
+//   arm1.setRotation(0,0);
+//   stopRotation = true;
+// }
+// void stopAnt(){
+//   arm1.setRotation(0,0);
+//   stopRotation = true;
+// }
